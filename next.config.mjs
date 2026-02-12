2cddf65f161d72815349100b1b34bb59b621587e
testtestingg <dridixhydra@gmail.com>
2026-02-12 23:53:36 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
   },
  
   eslint: {
